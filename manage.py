fcd2c7464f6da5339a4e54a2150dae210d864a49
Eric Sokolov <eric.sokolov.55@gmail.com>
2017-08-01 20:29:32 -0700

    added user/pass authentication

--- manage.py
+++ manage.py
@@ -4,6 +4,10 @@ from flask_script import Manager
 from blog import app
 from blog.database import Entry, session
 
+from getpass import getpass
+from werkzeug.security import generate_password_hash
+from blog.database import User
+
 manager = Manager(app)
 
 @manager.command
@@ -17,10 +21,27 @@ def seed():
 
     for i in range(0,25):
         entry = Entry(
-            title="bing Entry #{}".format(i),
+            title="blog Entry #{}".format(i),
             content=content
         )
         session.add(entry)
+    session.commit()
+
+@manager.command
+def adduser():
+    name = input("Name: ")
+    email = input("Email: ")
+    if session.query(User).filter_by(email=email).first():
+        print("User with that email address already exists")
+        return
+
+    password = ""
+    while len(password) < 8 or password != password_2:
+        password = getpass("Password: ")
+        password_2 = getpass("Re-enter password: ")
+    user = User(name=name,email=email,
+                password=generate_password_hash(password))
+    session.add(user)
     session.commit()
 
 if __name__ == "__main__":
